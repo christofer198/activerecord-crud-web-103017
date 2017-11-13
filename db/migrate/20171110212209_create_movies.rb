323e64befa30a24ae8299cc368a302446184f718
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 13:29:05 +0000

    automatically backed up by learn

--- db/migrate/20171110212209_create_movies.rb
+++ db/migrate/20171110212209_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |column|
       column.string :title
-      column.string :release_date
+      column.datetime :release_date
       column.string :director
       column.string :lead
       column.boolean :in_theaters
